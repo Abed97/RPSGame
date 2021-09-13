09c93370527fa205a3dd32cf74038fbb76e646f9
Abed Atassi <abed.s.atassi@gmail.com>
2021-09-12 21:18:12 -0400

    Initial Commit

---
+++ RPSGame/RPSGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSGameApp.swift
+//  RPSGame
+//
+//  Created by Abed Atassi on 2021-09-12.
+//
+
+import SwiftUI
+
+@main
+struct RPSGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
